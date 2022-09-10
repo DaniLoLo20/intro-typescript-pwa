7babcd0b7762442f6edf1c21b0ed5c1426fec45c
DaniLoLo20 <67060554+DaniLoLo20@users.noreply.github.com>
2022-09-10 12:04:34 -0500

    carpetas nuevas

---
+++ src/temas-ts/01-tippo-datos.ts
@@ -0,0 +1,18 @@
+
+
+console.log('Hola Mundo! 1002 Daniel Jimenez');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+
+var t1:number | string= 3;
+let t2:number | string=2;
+let activo:boolean=false;
+const t3=666;
+t1= 'www';
+t2= 'www';
+
+console.log(t1)
+console.log(t2)
